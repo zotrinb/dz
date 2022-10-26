513b69d3494cf4e0a013584d45421be68a811075
Zotrinb <zotrinb@gmail.com>
2022-10-26 03:02:10 +0300

    1
    re

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,13 @@
-const { parallel, series } = require('gulp');
+const { parallel, series, src } = require('gulp');
 
 const { views, clean, server, styles, images, fonts, scripts, svgSprite } = require('./gulp/tasks');
 
 const build = series(clean, parallel(views, styles, scripts, fonts, images, svgSprite));
 const start = series(build, server);
 
+const pug = () =>{
+  return src('src/views/pages/index.pug')
+}
 module.exports = {
   start,
   build,
